3f90a91d383d6969da098dfb582cdcfe5c95a422
vishnuv1228 <vishnuv1228@gmail.com>
2014-07-19 23:56:04 +0800

    Finished HTML+CSS+JQuery

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+//click to add and validate form
+	$('#button-add').click(function() {
+
+		if(!$.trim($('#add').val())) {
+			alert('Please enter an item.');
+		} 
+		else {
+				$("ul").append('<li class="add-more"><img class="x" src="css/image/big_x.png"/><img class="circle" src="css/image/circle.png"/>' + $('input#add').val() + '</li>');
+		}
+	});
+
+//delete list items
+	
+	$('ul').on('click', '.x', function(event) {
+		event.preventDefault();
+		$(this).parent().remove()
+});
+
+//cross out items
+	$('ul').on('click', '.circle', function() {
+		$(this).closest('li').toggleClass('done');
+	});
+// hiding empty, red block
+	$('.done').hide();
+
+});
